8fb7bf99bb14f2a0265d3ba8b22f4efc81a913d7
ZSI2017 <13407121461@163.com>
2018-01-22 00:25:52 +0800

    change bubble

--- dist/Bubble.js
+++ dist/Bubble.js
@@ -31,16 +31,16 @@ var Bubble = function() {
 // 渲染html 模板
 // 第一个参数是   传入的字符串，后面的参数对应的{0-10}，分别表示传入的变量。
 Bubble.format = function() {
- if (arguments.length == 0) {
-   return "";
- }
- var str = arguments[0];
- for (var i = 1, len = arguments.length; i < len; i++) {
-   var re = new RegExp('\\{' + (i - 1) + '\\}', 'gm');
-   str = str.replace(re, arguments[i]);
- }
- return str;
-}
+   if (arguments.length == 0) {
+     return "";
+   }
+   var str = arguments[0];
+   for (var i = 1, len = arguments.length; i < len; i++) {
+     var re = new RegExp('\\{' + (i - 1) + '\\}', 'gm');
+     str = str.replace(re, arguments[i]);
+   }
+   return str;
+};
 
 
 var class2type = {
@@ -53,31 +53,31 @@ var class2type = {
 
 "Boolean,Number,String,Function,Array,Date,RegExp,Window,Document,Arguments,NodeList".split(",").forEach(function(name,idx){
   class2type["[object "+name+"]"] = name.toLowerCase();
-})
+});
 
 Bubble.type=function(obj) {
   return obj == void 0 ?
                 "":obj == null?
                 String(obj) :
                 class2type[toString.call(obj)] || "object"
-}
+};
 
 
 // 判断 变量是否为数字。 可以是字符串，或者外形看上去象数字
 // 鸭子类型：  鸭子测试-- 当看到一只鸟走起来像鸭子，叫起来也像鸭子，那么这只鸟就可以被称为鸭子。
 Bubble.isNumeric = function(obj) {
-  return obj - parseFloat(obj) > = 0 ;
-}
+  return obj - parseFloat(obj) >= 0 ;
+};
 
 
 Bubble.isNumber = function(o){
   return '[object Number]' == {}.toString.call(o)
-}
+};
 
 
 Bubble.isDate = function(o) {
   return {}.toString.call(0) === "[object Date]" && o.toString() !== 'Invalid Date'
-}
+};
 
 
 
