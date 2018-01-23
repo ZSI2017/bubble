c26ee483944dc15009157a4fa042544c3ca9519e
ZSI2017 <13407121461@163.com>
2018-01-23 10:40:13 +0800

    change bubble.js

--- dist/Bubble.js
+++ dist/Bubble.js
@@ -44,6 +44,7 @@ Bubble.format = function() {
 
 
 var class2type = {
+  "null":"null",
   "NaN":"NaN",
   "undefined":"undefined",
   "[object global]":"window",
@@ -76,6 +77,94 @@ Bubble.isNumber = function(o){
 
 Bubble.isDate = function(o) {
   return {}.toString.call(0) === "[object Date]" && o.toString() !== 'Invalid Date'
+}
+
+
+Bubble.merge =  function(target) {
+  for (let i = 1, j = arguments.length; i < j; i++) {
+    let source = arguments[i] || {};
+    for (let prop in source) {
+      if (source.hasOwnProperty(prop)) {
+        let value = source[prop];
+        if (value !== undefined) {
+          target[prop] = value;
+        }
+      }
+    }
+  }
+  return target;
+};
+
+
+function xhrAdatpter(config){
+  return new Promise(function dispatchXhrRequest(resolve,reject){
+    var requestData = config.data;
+    var requestHeaders = config.headers;
+
+    var request = new XMLHttpRequest();
+    var loadEvent = 'onreadystatechange';
+    var xDomain = false;
+    request.open(config.method.toUpperCase(),buildURL(config.url,config.params,config.paramsSerizlizer),true);
+    request.timeout = config.timeout;
+    request.onload = function() {
+      if(!request || (request.readyState !== 4&&!xDomain)){
+        return;
+      }
+      if(request.status === 0 && !(request.responseURL && request.responseURL.indexOf("file:") === 0)){
+        return;
+      }
+      var responseData = !config.responseType || config.responseType === "test" ? request.responseText:request.response;
+      var response = {
+        data:responseData,
+        status:request.status === 1223 ?204:request.status,
+        statusText:request.status === 1223 ? 'No Content': request.statusText,
+        headers:responseHeaders,
+      }
+      settle(resolve,reject,response);
+      request = null
+    }
+    request.onerror = function handleError(){
+       reject(createError(new Error("Network Error"),config,null,request,null))
+       request = null;
+    }
+    request.ontimeout = function handleTimeout() {
+      reject(createError(new Error("timeout of " + config.timeout + "ms "),config,"ECONNABORTED",request))
+      request = null;
+    }
+    if(config.withCredentials) {
+      request.withCredentials = true;
+    }
+    request.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+    if(config.responseType) {
+      try {
+        request.responseType = config.responseType;
+      }catch(e) {
+        if(config.responseType !== "json") {
+          throw e;
+        }
+      }
+    }
+    request.send(requestData);
+  })
+}
+
+function settle(resolve,reject,response){
+  var validateStatus = response.config.validateStatus;
+  if(!response.status || ! !validateStatus || validateStatus(response.status)) {
+    resolve(response);
+  }else {
+      reject(createError(new Error(response.status),response.config,null,response.request,response))
+  }
+}
+
+function createError(error,config,code,request,response){
+    error.config = config;
+    if(code) {
+      error.code = code;
+    }
+    error.request = request;
+    error.response = response;
+    return error;
 }
 
 
